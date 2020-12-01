ef9cb597086a5cb1b714aa5e3e93b1fbe6519954
Robin <Robin>
2020-12-01 13:21:45 +0100

    1b

--- day_1/Program.cs
+++ day_1/Program.cs
@@ -8,22 +8,34 @@ namespace day_1
         static void Main(string[] args)
         {
             string[] input = File.ReadAllLines("input.txt");
-            
+
             for (int i = 0; i < input.Length; i++)
             {
-                string line = input[i];
-                string find = Convert.ToString(2020 - Convert.ToInt16(line));
+                int line = Convert.ToInt16(input[i]);
+                int find = 2020 - line;
                 for (int j = i + 1; j < input.Length; j++)
                 {
-                    string possibleMatch = input[j];
+                    int possibleMatch = Convert.ToInt16(input[j]);
                     if (find.Equals(possibleMatch))
                     {
                         System.Console.WriteLine(line + " + " + find + " = 2020");
-                        System.Console.WriteLine("Answer: " + Convert.ToInt16(line) * Convert.ToInt16(find));
-                        return;
+                        System.Console.WriteLine("Part one answer: " + line * possibleMatch);
+                    }
+                    else if (possibleMatch < find)
+                    {
+                        int lastFind = 2020 - line - possibleMatch;
+                        for (int k = j + 1; k < input.Length; k++)
+                        {
+                            int possibleLastMatch = Convert.ToInt16(input[k]);
+                            if (lastFind.Equals(possibleLastMatch))
+                            {
+                                System.Console.WriteLine(line + " + " + possibleMatch + " + " + possibleLastMatch + " = 2020");
+                                System.Console.WriteLine("Part two answer: " + line * possibleMatch * possibleLastMatch);
+                                return;
+                            }
+                        }
                     }
                 }
-
             }
         }
     }
