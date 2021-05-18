486ed224af9ea05ca0601c80a755c45e18a6b045
Robin Becker <robin.becker@fore.dev>
2021-05-18 15:23:55 +0200

    day 10 part 2

--- day_10/Program.cs
+++ day_10/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Numerics;
+using System.Collections.Generic;
 using System.IO;
 using System;
 
@@ -15,6 +16,7 @@ namespace day_10
             foreach (string row in input)
                 adapters.Add(int.Parse(row));
 
+            adapters.Insert(0, 0);
             adapters.Sort();
 
             int jolt = 0, one_jolt = 0, three_jolt = 1;
@@ -30,6 +32,31 @@ namespace day_10
             }
             System.Console.WriteLine("Part one: " + one_jolt + "+" + three_jolt + " = " + (one_jolt * three_jolt));
 
+            BigInteger branches = new BigInteger(1);
+
+            for (int i = 0; i < adapters.Count - 1; i++)
+            {
+                int groupSize = 0;
+                for (int n = 2; i + n < adapters.Count && adapters[i + n] - adapters[i] == n; n++)
+                {
+                    groupSize++;
+                }
+                if (groupSize == 1)
+                {
+                    branches *= 2;
+                }
+                else if (groupSize == 2)
+                {
+                    branches *= 4;
+                }
+                else if (groupSize == 3)
+                {
+                    branches *= 7;
+                }
+                
+                i += groupSize;
+            }
+            System.Console.WriteLine("Part two: " + branches);
         }
     }
 }
